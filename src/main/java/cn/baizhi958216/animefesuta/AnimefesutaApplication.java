29e58938db65949d03da9ac8cadeb76e8f9e06c1
baizhi958216 <1475289190@qq.com>
2024-01-27 20:14:15 +0800

    feat: 初始化Spring项目
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ src/main/java/cn/baizhi958216/animefesuta/AnimefesutaApplication.java
@@ -0,0 +1,13 @@
+package cn.baizhi958216.animefesuta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimefesutaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimefesutaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/baizhi958216/animefesuta/AnimefesutaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.baizhi958216.animefesuta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimefesutaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
