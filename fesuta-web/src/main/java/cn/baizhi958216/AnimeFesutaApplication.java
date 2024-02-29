2320fede4a0c5cea6f9e5d7295848a94cf0bcefd
baizhi958216 <1475289190@qq.com>
2024-03-01 03:22:38 +0800

    feat: 新增模块
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ fesuta-web/src/main/java/cn/baizhi958216/AnimeFesutaApplication.java
@@ -0,0 +1,11 @@
+package cn.baizhi958216;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeFesutaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AnimeFesutaApplication.class, args);
+    }
+}
